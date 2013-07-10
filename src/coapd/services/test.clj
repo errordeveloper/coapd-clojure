29fb075feb2e9648c17b341fe8e204014a496c51
Ilya Dmitrichenko <ilya@xively.com>
2013-07-10 18:02:45 +0100

    It's actuallly much simpler

--- src/coapd/services/test.clj
+++ src/coapd/services/test.clj
@@ -1,20 +1,13 @@
-(ns coapd.services.test.class
+(ns coapd.services.test
   (:require [clojure.tools.logging :as log])
   (:import [de.uniluebeck.itm.ncoap.message CoapRequest CoapResponse MessageDoesNotAllowPayloadException]
            [de.uniluebeck.itm.ncoap.message.header Code]
-           ;[de.uniluebeck.itm.ncoap.message Option UintOption]
            [de.uniluebeck.itm.ncoap.message.options OptionRegistry]
-           [de.uniluebeck.itm.ncoap.application.server.webservice NotObservableWebService])
-  (gen-class :extends NotObservableWebService :main false))
-
-(defn -processCoapRequest [res req addr]
-  (log/info "Hello!!")
-  (.set res (new CoapResponse Code/INTERNAL_SERVER_ERROR_500)))
-
-(ns coapd.services.test
-  (:use coapd.services.test.class))
+           [de.uniluebeck.itm.ncoap.application.server.webservice NotObservableWebService]))
 
 (defn service [servicePath initialStatus]
   (proxy [de.uniluebeck.itm.ncoap.application.server.webservice.NotObservableWebService] [servicePath initialStatus]
-    (processCoapRequest [res req addr] (-processCoapRequest this res req addr))))
+    (processCoapRequest [res req addr]
+      (log/info "Hello!!")
+      (.set res (new CoapResponse Code/INTERNAL_SERVER_ERROR_500)))))
 
